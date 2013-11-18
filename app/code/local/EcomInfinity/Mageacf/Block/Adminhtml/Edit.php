a732306281d65c947b1ee6f5ff18063a49569def
crossgate9 <crossgate9@gmail.com>
2013-11-18 19:19:56 +0800

    BIG commit. [add] jquery and fancybox support; [add] basic ui layout of backend management; [add] sketch up for frontend framework; [bug fix] not define mapping of model and data table.

--- app/code/local/EcomInfinity/Mageacf/Block/Adminhtml/Edit.php
+++ app/code/local/EcomInfinity/Mageacf/Block/Adminhtml/Edit.php
@@ -4,4 +4,42 @@ class EcomInfinity_Mageacf_Block_Adminhtml_Edit extends Mage_Core_Block_Template
     public function _prepareLayout() {
         return parent::_prepareLayout();
     }
+
+    public function getACFBaseUrl() {
+        return $this->getUrl('adminhtml/mageacf');
+    }
+
+    public function getAllColors() {
+        $_config = Mage::getModel('eav/config');
+        $_attribute = $_config->getAttribute(Mage_Catalog_Model_Product::ENTITY, 'color');
+
+        $_values = $_attribute->setStoreId(1)->getSource()->getAllOptions();
+
+        foreach ($_values as $_key => $_color) {
+            if (strpos($_color['label'], ';') !== false) {
+                $_c = explode(';', $_color['label']);
+                $_values[$_key]['code'] = $_c[0];
+                $_values[$_key]['label'] = $_c[1];
+            } else {
+                $_values[$_key]['code'] = '#fff';
+            }
+        }
+
+        return $_values;
+    }
+
+    public function getAllStores() {
+        $_stores = Mage::getSingleton('adminhtml/system_store')->getStoreValuesForForm(false, true);
+
+        return $_stores;
+    }
+
+    public function getAllACFs() {
+        $_acfs = Mage::getModel('mageacf/group')->getCollection();
+        $_result = array();
+        foreach ($_acfs as $_acf) {
+            $_result[] = $_acf->getData();
+        }
+        return $_result;
+    }
 }
---
+++ skin/adminhtml/default/default/js/ecominfinity/mageacf/global.js
@@ -0,0 +1,88 @@
+jQuery.noConflict();
+
+var ACF = (function($) {
+    var c = function(url, formKey) {
+        this.baseUrl = url;
+        this.formKey = formKey;
+        this.ajax = function(url, data, callback) {
+            $.extend(data, {formKey: this.formKey});
+            $.ajax({
+                url: url,
+                data: data,
+                type: 'POST',
+                beforeSend: function() { $.fancybox.showLoading(); }
+            }).done(function(response) {
+                callback(response);
+                $.fancybox.hideLoading();
+            });
+        };
+    };
+
+    c.prototype.create = function(name, color, store_view, list, callback) {
+        var url = this.baseUrl + 'create';
+        var data = {
+            name: name,
+            color: color,
+            storeview: store_view
+        };
+        this.ajax(url, data, callback);
+    };
+
+    c.prototype.delete = function(id, callback) {
+        var url = this.baseUrl + 'delete';
+        var data = {
+            id: id
+        };
+        this.ajax(url, data, callback);
+    };
+
+    return c;
+
+})(jQuery);
+var acf;
+
+(function($) {
+    $(function() {
+
+        acf = new ACF(acfBaseUrl, formKey);
+
+        // color attribute hover effect
+        $('#select-color-attribute').on('mouseenter', 'option', function(e) {
+            var code = $(this).data('code');
+            var name = $(this).html();
+            $('.color-attribute-preview .color-box').css('background', code);
+            $('.color-attribute-preview .name').html(name);
+            $(this).css('background', '#ddd');
+        });
+
+        $('#select-color-attribute').on('mouseleave', 'option', function(e) {
+            $(this).css('background', '');
+        });
+
+        // create new action
+        $('.btn-create-new').on('click', function() {
+            $('#input-group-name').val('');
+            $('#input-group-color-code').val('');
+            $('#select-color-attribute').find('option').attr('selected', false);
+            $('.modify-panel').hide();
+            $('.create-panel').show();
+            return false;
+        });
+
+        $('.create-panel .btn-action').on('click', function() {
+            var name = $('#input-group-name').val(),
+                color = $('#input-group-color-code').val(),
+                storeView = $('#select-store-view').val(),
+                list = null;
+            acf.create(name, color, storeView, list, function(response) {
+                console.log(response);
+            });
+            return false;
+        });
+
+        $('.create-panel .btn-cancel').on('click', function() {
+            $('.create-panel').hide();
+            return false;
+        });
+    });
+})(jQuery);
